7bbbb64ba618c8befeae9e54a6870144445b2ddd
Antonio <antojrd@gmail.com>
2024-09-08 14:46:11 +0200

    Handle editor user on update

--- customers/serializers.py
+++ customers/serializers.py
@@ -1,16 +1,24 @@
 from rest_framework import serializers
 
 from customers.models import Customer
+from users.models import User
+
+
+class RelatedUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'username', 'is_superuser']
 
 
 class CustomerSerializer(serializers.ModelSerializer):
+    author = RelatedUserSerializer(read_only=True)
+    editor = RelatedUserSerializer(read_only=True)
     class Meta:
         model = Customer
         fields = '__all__'
-        depth = 1
 
 
-class CustomerCreateSerializer(serializers.ModelSerializer):
+class CustomerCreateUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['name', 'last_name', 'photo']
--- customers/views.py
+++ customers/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthentic
 from rest_framework.response import Response
 
 from customers.models import Customer
-from customers.serializers import CustomerSerializer, CustomerCreateSerializer
+from customers.serializers import CustomerSerializer, CustomerCreateUpdateSerializer
 
 
 class AccountViewSet(viewsets.ModelViewSet):
@@ -18,12 +18,20 @@ class AccountViewSet(viewsets.ModelViewSet):
         if self.action in ['list', 'retrieve']:
             return CustomerSerializer
         if self.action in ['create', 'partial_update', 'update']:
-            return CustomerCreateSerializer
+            return CustomerCreateUpdateSerializer
         return CustomerSerializer
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save(author=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        print(f'Data {request.data}')
+        serializer = self.get_serializer(data=request.data, instance=self.get_object())
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(editor=request.user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
