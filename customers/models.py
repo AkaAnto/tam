a9e6346389cbcaae788b4d5b469722eed80c196e
Antonio <antojrd@gmail.com>
2024-09-17 12:00:28 -0400

    upload customer images to s3

--- customers/models.py
+++ customers/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from TAM import settings
+from TAM.custom_storage import MediaStorage
 
 
 class Customer(models.Model):
   name = models.CharField(max_length=255, )
   last_name = models.CharField(max_length=255)
-  photo = models.ImageField(upload_to='uploads/images', null=True, blank=True)
+  photo = models.ImageField(upload_to='uploads/images', storage=MediaStorage, null=True, blank=True)
   author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name="customers")
   editor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name="edited_customers", null=True, blank=True, default=None)
   created_on = models.DateTimeField(auto_now=True)
